197c0da29e13bfac85d6c7c37a4a1b5d7b92ce37
markeu <ucheuzochukwumark@gmail.com>
2021-01-18 18:26:50 +0100

    set up queries and function call

---
+++ db/database.js
@@ -0,0 +1,36 @@
+var sqlite3 = require('sqlite3').verbose()
+
+const DBSOURCE = 'db.sqlite';
+
+const DB_SCHEMA = `CREATE TABLE IF NOT EXISTS actors (
+  id integer NOT NULL PRIMARY KEY,
+  login text  NOT NULL,
+  avatar_url text  NOT NULL
+);
+CREATE TABLE IF NOT EXISTS repos (
+  id integer NOT NULL PRIMARY KEY,
+  name  VARCHAR(75)  NOT NULL,
+  url  VARCHAR(125)  NOT NULL,
+  actor_id integer
+);
+CREATE TABLE IF NOT EXISTS events (
+  id integer  NOT NULL PRIMARY KEY,
+  type VARCHAR(75)  NOT NULL,
+  created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  actor_id integer,
+  repo_id integer
+)`;
+
+let db = new sqlite3.Database(DBSOURCE, (err) => {
+    if (err) {
+        console.error('Error when creating the database', err.message)
+        throw err
+    } else {
+        console.log('Connected to ' + DBSOURCE + ' database.')
+        db.exec(DB_SCHEMA, (err) => {
+            if (err) { throw new Error(err) }
+        });
+    }
+})
+
+module.exports = db
---
+++ db/dbQuery.js
@@ -0,0 +1,44 @@
+var DB = require('./db')
+const Promise = require('bluebird')
+
+const ALL_EVENT_SQL = `SELECT events.*, actors.id AS actor_id, actors.login, actors.avatar_url, repos.id as repo_id, repos.url, repos.name
+FROM events
+INNER JOIN actors ON actors.id = events.actor_id
+INNER JOIN repos ON repos.id = events.repo_id
+ORDER BY id ASC`;
+
+const GET_EVENTS_BY_ACTOR = `SELECT events.*, actors.id AS actor_id, actors.login, actors.avatar_url, repos.id as repo_id, repos.url, repos.name
+FROM events
+INNER JOIN actors ON actors.id = events.actor_id
+INNER JOIN repos ON repos.actor_id = events.actor_id
+WHERE events.actor_id = ?
+ORDER BY id ASC`;
+
+const GET_ALL_ACTORS = `SELECT actors.id, actors.login, actors.avatar_url , events.created_at AS created_at , count(events.id) AS event_count FROM actors
+INNER JOIN events ON events.actor_id = actors.id
+GROUP BY login
+ORDER BY event_count DESC, created_at DESC, login DESC;`;
+
+
+const GET_ACTORS_BY_STREAK = `SELECT actors.id, actors.login, actors.avatar_url, events.created_at AS created_at, COUNT(events.actor_id) AS streak FROM actors
+INNER JOIN events  ON  events.actor_id = actors.id
+WHERE login IS NOT NULL
+GROUP BY login
+ORDER BY streak DESC, created_at DESC, login DESC;`;
+
+
+module.exports = {
+
+    DB,
+    ALL_EVENT_SQL,
+    CHECK_IF_ACTOR_EXIST: (id) => `SELECT count(*) AS count FROM events WHERE id=${id}`,
+    INSERT_ACTOR_SQL: 'REPLACE INTO actors (id, login, avatar_url) VALUES (?, ?, ?);',
+    INSERT_REPO_SQL: 'REPLACE INTO repos (id, name, url, actor_id) VALUES (?, ?, ?, ?);',
+    INSERT_EVENT_SQL: 'INSERT INTO events (id, type,created_at, actor_id, repo_id) VALUES (?, ?,?, ?, ?);',
+    GET_EVENTS_BY_ACTOR,
+    DROP_ALL_RECORDS: 'DELETE FROM events; DELETE FROM actors; DELETE FROM repos',
+    GET_ALL_ACTORS,
+    UPDATE_ACTOR: 'UPDATE actors SET avatar_url = ? WHERE id = ?',
+    GET_ACTORS_BY_STREAK
+
+}
